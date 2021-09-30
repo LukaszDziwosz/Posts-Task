72ab576575d486078bade25fc91998610dc5d434
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-09-30 19:42:05 +0200

    Initial Commit

---
+++ Posts_Task/Posts_TaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  Posts_TaskApp.swift
+//  Posts_Task
+//
+//  Created by Lukasz Dziwosz on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Posts_TaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
